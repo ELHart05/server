ivangegovdve-sudo/server-1

feat(settings): Rework personal info into a sectioned single-column layout
Personal info is reorganized into clear sections in a single column instead
of a multi-column grid of separately mounted widgets, with fields adopting a
consistent modern look in line with the settings redesign.

Signed-off-by: nfebe <fenn25.fn@gmail.com>

apps/settings/src/components/PersonalInfo/BirthdaySection.vue
@@ -3,22 +3,21 @@
  - SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 <template>
-	<section>
-		<HeaderBar
-			:scope="birthdate.scope"
-			:input-id="inputId"
-			:readable="birthdate.readable" />
-
-		<NcDateTimePickerNative
-			:id="inputId"
-			type="date"
-			label=""
-			:model-value="value"
-			@input="onInput" />
-
-		<p class="property__helper-text-message">
-			{{ t('settings', 'Enter your date of birth') }}
-		</p>
+	<section class="property-section">
+		<div class="property">
+			<NcDateTimePickerNative
+				:id="inputId"
+				class="property__field"
+				type="date"
+				:label="birthdate.readable"
+				:model-value="value"
+				@input="onInput" />
+			<FederationControl
+				class="property__scope"
+				:readable="birthdate.readable"
+				:scope="birthdate.scope"
+				@update:scope="onScopeChange" />
+		</div>
 	</section>
 </template>
 
@@ -26,7 +25,7 @@
 import { loadState } from '@nextcloud/initial-state'
 import debounce from 'debounce'
 import NcDateTimePickerNative from '@nextcloud/vue/components/NcDateTimePickerNative'
-import HeaderBar from './shared/HeaderBar.vue'
+import FederationControl from './shared/FederationControl.vue'
 import { NAME_READABLE_ENUM } from '../../constants/AccountPropertyConstants.js'
 import { savePrimaryAccountProperty } from '../../service/PersonalInfo/PersonalInfoService.js'
 import { handleError } from '../../utils/handlers.js'
@@ -38,7 +37,7 @@ export default {
 
 	components: {
 		NcDateTimePickerNative,
-		HeaderBar,
+		FederationControl,
 	},
 
 	data() {
@@ -78,6 +77,10 @@ export default {
 	},
 
 	methods: {
+		onScopeChange(scope) {
+			this.birthdate.scope = scope
+		},
+
 		onInput(e) {
 			this.value = e
 			this.debouncePropertyChange(this.value)
@@ -118,18 +121,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-section {
-	padding: 10px 10px;
+.property-section {
+	padding: 6px 0;
+}
 
-	:deep(button:disabled) {
-		cursor: default;
+.property {
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	gap: 8px;
+
+	&__field {
+		flex: 1 1 auto;
+		min-width: 0;
 	}
 
-	.property__helper-text-message {
-		color: var(--color-text-maxcontrast);
-		padding: 4px 0;
+	&__scope {
+		flex: 0 0 44px;
 		display: flex;
-		align-items: center;
+		justify-content: center;
 	}
 }
 </style>

ELHart05/server

apps/settings/src/components/PersonalInfo/BirthdaySection.vue
@@ -3,18 +3,21 @@
  - SPDX-License-Identifier: AGPL-3.0-or-later
 -->
 <template>
-	<section>
-		<HeaderBar
-			:scope="birthdate.scope"
-			:input-id="inputId"
-			:readable="birthdate.readable" />
-
-		<NcDateTimePickerNative
-			:id="inputId"
-			type="date"
-			label=""
-			:model-value="timezoneAdjustedValue"
-			@input="onInput" />
+	<section class="property-section">
+		<div class="property">
+			<NcDateTimePickerNative
+				:id="inputId"
+				class="property__field"
+				type="date"
+				:label="birthdate.readable"
+				:model-value="timezoneAdjustedValue"
+				@input="onInput" />
+			<FederationControl
+				class="property__scope"
+				:readable="birthdate.readable"
+				:scope="birthdate.scope"
+				@update:scope="onScopeChange" />
+		</div>
 
 		<p class="property__helper-text-message">
 			{{ t('settings', 'Enter your date of birth') }}
@@ -26,7 +29,7 @@
 import { loadState } from '@nextcloud/initial-state'
 import debounce from 'debounce'
 import NcDateTimePickerNative from '@nextcloud/vue/components/NcDateTimePickerNative'
-import HeaderBar from './shared/HeaderBar.vue'
+import FederationControl from './shared/FederationControl.vue'
 import { NAME_READABLE_ENUM } from '../../constants/AccountPropertyConstants.js'
 import { savePrimaryAccountProperty } from '../../service/PersonalInfo/PersonalInfoService.js'
 import { handleError } from '../../utils/handlers.js'
@@ -48,7 +51,7 @@ export default {
 
 	components: {
 		NcDateTimePickerNative,
-		HeaderBar,
+		FederationControl,
 	},
 
 	data() {
@@ -97,6 +100,10 @@ export default {
 	},
 
 	methods: {
+		onScopeChange(scope) {
+			this.birthdate.scope = scope
+		},
+
 		onInput(e) {
 			const day = e.getDate().toString().padStart(2, '0')
 			const month = (e.getMonth() + 1).toString().padStart(2, '0')
@@ -140,14 +147,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-section {
-	padding: 10px 10px;
+.property-section {
+	padding: 6px 0;
+}
 
-	:deep(button:disabled) {
-		cursor: default;
+.property {
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	gap: 8px;
+
+	&__field {
+		flex: 1 1 auto;
+		min-width: 0;
 	}
 
-	.property__helper-text-message {
+	&__scope {
+		flex: 0 0 44px;
+		display: flex;
+		justify-content: center;
+	}
+
+	&__helper-text-message {
 		color: var(--color-text-maxcontrast);
 		padding: 4px 0;
 		display: flex;